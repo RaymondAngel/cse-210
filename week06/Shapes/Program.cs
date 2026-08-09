9ccf3e332d25f6fc614cd1404e061a9c421f13fa
RaymondAngel <ramande@byupathway.edu>
2026-08-09 02:35:08 -0700

    Completed Week 6 Learning Activity Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,19 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
